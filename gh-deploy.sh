aeaf5110f3f0eef0d5ac0e5522fbc5dd7ac67d2f
Umayr Shahid <umayrr@hotmail.co.uk>
2014-09-19 20:14:16 +0500

    modified copy task to move fonts.json to dist

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if [ -z "$1" ]
 then
-  echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "Provide the name of folder you want to deploy to GitHub Pages."
   exit 1
 fi
+
+echo "Deleting existing remote gh-pages branch...";
+git push origin :gh-pages
+
+echo "Deploying to Github Pages...";
 git subtree push --prefix $1 origin gh-pages
